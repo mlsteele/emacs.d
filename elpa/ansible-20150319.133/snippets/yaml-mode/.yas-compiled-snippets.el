eed6bd70f21e6616d3c262798b3ab0d94c614b4f
Miles Steele <miles@milessteele.com>
2016-03-02 00:20:13 -0500

    checkin elpa

---
+++ elpa/ansible-20150319.133/snippets/yaml-mode/.yas-compiled-snippets.el
@@ -0,0 +1,2 @@
+;;; Compiled snippets and support files for `yaml-mode'
+;;; Do not edit! File generated at Sat Feb  6 00:50:41 2016
---
+++ elpa/powerline-evil-20140517.1/powerline-evil-autoloads.el
@@ -0,0 +1,48 @@
+;;; powerline-evil-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "powerline-evil" "powerline-evil.el" (21888
+;;;;;;  37515 81383 116000))
+;;; Generated autoloads from powerline-evil.el
+
+(autoload 'powerline-evil-face "powerline-evil" "\
+Function to select appropriate face based on `evil-state'.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "powerline-evil-themes" "powerline-evil-themes.el"
+;;;;;;  (21888 37515 9379 289000))
+;;; Generated autoloads from powerline-evil-themes.el
+
+(autoload 'powerline-evil-center-color-theme "powerline-evil-themes" "\
+Powerline's center-evil them with the evil state in color.
+
+\(fn)" t nil)
+
+(autoload 'powerline-evil-vim-theme "powerline-evil-themes" "\
+Powerline's Vim-like mode-line with evil state at the beginning.
+
+\(fn)" t nil)
+
+(autoload 'powerline-evil-vim-color-theme "powerline-evil-themes" "\
+Powerline's Vim-like mode-line with evil state at the beginning in color.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("powerline-evil-pkg.el") (21888 37515
+;;;;;;  230241 564000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; powerline-evil-autoloads.el ends here
