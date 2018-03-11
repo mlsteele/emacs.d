d4539e258d4747c81b3f40288d2503ac5273d878
Miles Steele <miles@milessteele.com>
2018-03-11 11:36:52 -0400

    make ocaml conditional

--- config/70-ocaml.el
+++ config/70-ocaml.el
@@ -1,11 +1,13 @@
 ;; Add opam emacs directory to the load-path
-(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+(when (executable-find "opam")
+  (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 
-(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 
-;; Load merlin-mode
-(require 'merlin)
+  ;; Load merlin-mode
+  (require 'merlin)
 
-;; Start merlin on ocaml files
-(add-hook 'tuareg-mode-hook 'merlin-mode t)
-(add-hook 'caml-mode-hook 'merlin-mode t)
+  ;; Start merlin on ocaml files
+  (add-hook 'tuareg-mode-hook 'merlin-mode t)
+  (add-hook 'caml-mode-hook 'merlin-mode t))
+
